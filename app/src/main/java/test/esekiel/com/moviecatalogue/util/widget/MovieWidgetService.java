c44fbf8ef2ec107202a5d6b57076d42b51c2fa23
esekiel <delusionalcode@gmail.com>
2019-10-19 15:57:58 +0700

    add movie widget

---
+++ app/src/main/java/test/esekiel/com/moviecatalogue/util/widget/MovieWidgetService.java
@@ -0,0 +1,16 @@
+package test.esekiel.com.moviecatalogue.util.widget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class MovieWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new MovieRemoteViewsFactory(this.getApplicationContext(), intent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
